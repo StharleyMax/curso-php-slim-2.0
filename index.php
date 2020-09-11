58fa7cb6c66818711c0de3de82c132e44d41f2e4
Stharley Maxwell <stharleymax@gmail.com>
2020-09-11 13:28:15 -0300

    configuring vendor plus SQL class

--- index.php
+++ index.php
@@ -5,7 +5,10 @@ $app = new \Slim\Slim();
 
 $app->config('debug', true);
 $app->get('/', function(){
-  echo "ok";
+  $sql = new Hcode\DB\sql();
+
+  $results = $sql->select("select * from tb_users");
+  echo json_encode($results);
 });
 
-$app->run();
+$app->run(); 
